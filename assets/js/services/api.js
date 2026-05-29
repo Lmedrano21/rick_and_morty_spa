1b8f0e70f3561d765d0c93a7eb76533bb13b94d6
Alejandra Jimenez <alejandrajd16@gmail.com>
2026-05-28 21:33:21 -0500

    funciones locations and episodes

--- assets/js/services/api.js
+++ assets/js/services/api.js
@@ -19,3 +19,31 @@ export async function getCharacters() {
         return [];
     }
 }
+
+/**
+ *  obtener episodes
+ */
+export async function getEpisodes() {
+    try {
+        const response = await httpClient.get('/episode');
+        return response.data.results;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }   
+
+}
+
+/**
+ * obtener locations
+ */
+
+export async function getLocations() {
+    try {
+        const response = await httpClient.get('/location');
+        return response.data.results;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
